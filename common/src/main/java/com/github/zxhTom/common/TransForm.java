5e890f6535fce59ffa3250e9e5328bc54a29cc48
zxh_zhitong <870775401@qq.com>
2018-01-30 15:55:03 +0800

    add

--- common/src/main/java/com/github/zxhTom/common/TransForm.java
+++ common/src/main/java/com/github/zxhTom/common/TransForm.java
@@ -3,11 +3,18 @@ package com.github.zxhTom.common;
 public class TransForm {
 
 	private static TransForm transForm = new TransForm();
-	
-	public static TransForm getInstance(){
+
+	public static TransForm getInstance() {
 		return transForm;
 	}
-	public void sayHello(){
+
+	public void sayHello() {
+
 		System.out.println("Hello World");
+	}
+
+	public void sayHellos() {
+
+		System.out.println("Hello Worlds");
 	}
 }
